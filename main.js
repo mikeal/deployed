c33f485207e31c4b02d0054e5dcbdf64fd92ac49
Mikeal Rogers <mikeal.rogers@gmail.com>
2012-01-09 17:24:48 -0800

    improvements.

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ var pushover = require('pushover')
   , gitemit = require('git-emit')
   , path = require('path')
   , gits = {}
-  , repos = pushover(path.join(__dirname, tests))
+  , repos = pushover(path.join(__dirname, 'test'))
   ;
 
 // repos.autoCreate = false
 repos.on('push', function (repo) {
-    
+    console.log('asdf')
 });
 
 repos.list(function (err, reps) {
   reps.forEach(function (r) {
     gits[r] = gitemit(path.join(__dirname, r))
     gits[r].on('update', function (update) {
-      console.log(update)
+      console.log('update', update)
       update.reject('test')
     })
   })
